c4052eb333f7cbc156ccdf63ee7cddef5ab66069
ChristopherBurns1996 <Christopher.Burns@academytrainee.com>
2019-09-23 18:25:37 +0100

    Initial commit

---
+++ src/test/java/com/qa/NavigationSeleniumTests.java
@@ -0,0 +1,29 @@
+package com.qa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class NavigationSeleniumTests {
+    ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Documents\\projectSeleniumTests\\src\\test\\java\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void nav1(){
+
+    }
+}
