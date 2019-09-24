58f34f18127b85e3354f62484cf85c71b878e6c0
ChristopherBurns1996 <Christopher.Burns@academytrainee.com>
2019-09-24 14:09:57 +0100

    "committing thirteenth navigation test from update publisher page page to display publishers"

--- src/test/java/com/qa/NavigationSeleniumTests.java
+++ src/test/java/com/qa/NavigationSeleniumTests.java
@@ -203,4 +203,18 @@ public class NavigationSeleniumTests {
         WebElement title = driver.findElementById("pageTitle");
         assertEquals("Update Publisher Information", title.getText());
     }
+
+    @Test
+    public void navUPtoDPTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("http://34.89.59.112/UpdatePublisher.html?publisherid=13");
+        Thread.sleep(1000);
+
+        WebElement dpButton = driver.findElementById("dpButton");
+        dpButton.click();
+        Thread.sleep(1000);
+
+        WebElement title = driver.findElementById("pageTitle");
+        assertEquals("Displaying All Publishers", title.getText());
+    }
 }
